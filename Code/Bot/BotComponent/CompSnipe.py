c9905a392e6531aeac8c50cf10567b5c8bb0dc6b
Rio <rio_ho@vgame.com.tw>
2023-10-02 23:37:28 +0800

    狙擊刪除訊息加入貼圖、圖片，編輯訊息判斷

--- Code/Bot/BotComponent/CompSnipe.py
+++ Code/Bot/BotComponent/CompSnipe.py
@@ -22,38 +22,59 @@ class CompSnipe(CompBotBase):
     async def ReplyRemoveMessage(self, message: discord.Message):
         # TODO lock
         removePayload: discord.RawMessageDeleteEvent = self.removeMessages[message.channel.id]
-        if removePayload.cached_message != None:
-            removeMsg = removePayload.cached_message
-            title = f"{removeMsg.author.display_name}({removeMsg.author.name})"
-            embed = discord.Embed(title=title, color=0x3498db)
-            embed.add_field(name="刪除的訊息", value=removeMsg.content)
-            await message.reply(embed=embed, mention_author=False)
-        else:
+        if removePayload.cached_message == None:
             await message.reply(content="找不到刪除的訊息", mention_author=False)
+        removeMsg = removePayload.cached_message
+        embeds = []
+        title = f"{removeMsg.author.display_name}({removeMsg.author.name})"
+        firstEmbed = discord.Embed(title=title, color=0x3498db)
+        firstEmbed.add_field(name="刪除的訊息", value=removeMsg.content)
+        embeds.append(firstEmbed)
+        emptyImage = True
+        allUrls = []
+        for attach in removeMsg.attachments:
+            allUrls.append(attach.url)
+        stickers = []
+        guildStickers = await self.botClient.GetGuild().fetch_stickers()
+        strickerIds = [sticker.id for sticker in guildStickers]
+        for getSticker in removeMsg.stickers:
+            if getSticker.id in strickerIds:
+                stickers.append(getSticker)
+            else:
+                allUrls.append(getSticker.url)
+        for url in allUrls:
+            if emptyImage:
+                embeds[0].set_image(url=url)
+                emptyImage = False
+            else:
+                addEmbed = discord.Embed()
+                addEmbed.set_image(url=url)
+                addEmbed.add_field(name="刪除的訊息", value=removeMsg.content)
+                embeds.append(addEmbed)
+        await message.reply(embeds=embeds, mention_author=False, stickers=stickers)
 
     async def ReplyEditMessage(self, message: discord.Message):
         # TODO lock
         editPayload: discord.RawMessageUpdateEvent = self.editMessages[message.channel.id]
-        if editPayload.cached_message != None:
-            editMsg = editPayload.cached_message
-            title = f"{editMsg.author.display_name}({editMsg.author.name})"
-            embed = discord.Embed(title=title, color=0x3498db)
-            embed.add_field(name="原本的訊息", value=editMsg.content, inline=False)
-            if "content" in editPayload.data:
-                embed.add_field(
-                    name="修改的訊息", value=editPayload.data["content"], inline=False)
-            await message.reply(embed=embed, mention_author=False)
-        else:
+        if editPayload.cached_message == None:
             await message.reply(content="找不到編輯的訊息", mention_author=False)
+            return
+        editMsg = editPayload.cached_message
+        title = f"{editMsg.author.display_name}({editMsg.author.name})"
+        embed = discord.Embed(title=title, color=0x3498db)
+        embed.add_field(name="原本的訊息", value=editMsg.content, inline=False)
+        if "content" in editPayload.data:
+            embed.add_field(
+                name="修改的訊息", value=editPayload.data["content"], inline=False)
+        await message.reply(embed=embed, mention_author=False)
 
     async def on_raw_message_edit(self, payload: discord.RawMessageUpdateEvent):
-        # TODO 檢查嵌入式
         if payload.cached_message != None:
             if payload.cached_message.author.bot:
                 self.LogI("on_raw_message_edit skip bot")
                 return
-        self.editMessages[payload.channel_id] = payload
-        # print("on_raw_message_edit")
+        if "member" in payload.data:
+            self.editMessages[payload.channel_id] = payload
 
     async def on_message(self, message: discord.Message) -> None:
         if re.match("!snipe", message.content):
@@ -74,4 +95,3 @@ class CompSnipe(CompBotBase):
                 self.LogI("on_raw_message_delete skip bot")
                 return
         self.removeMessages[payload.channel_id] = payload
-        # print("on_raw_message_delete")
