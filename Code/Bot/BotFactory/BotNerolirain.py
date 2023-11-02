c2f75d3bcc6b148d92a0bdd5ee6c954202ea2567
Rio <rio_ho@vgame.com.tw>
2023-11-02 20:00:40 +0800

    隨機指令、成語更新

--- Code/Bot/BotFactory/BotNerolirain.py
+++ Code/Bot/BotFactory/BotNerolirain.py
@@ -1,14 +1,15 @@
 
 
 from Bot.BotFactory.BotBase import BotBase
-from Bot.BotComponent.CompLog import CompLog
-from Bot.BotComponent.CompUsers import CompUsers
-from Bot.BotComponent.CompUpdateDatabase import CompUpdateDatabase
-from Bot.BotComponent.CompSnipe import CompSnipe
 from Bot.BotComponent.CompClose import CompClose
 from Bot.BotComponent.CompDraw import CompDraw
-from Bot.BotComponent.CompSystem import CompSystem
 from Bot.BotComponent.CompIdiom import CompIdiom
+from Bot.BotComponent.CompLog import CompLog
+from Bot.BotComponent.CompRandom import CompRandom
+from Bot.BotComponent.CompSnipe import CompSnipe
+from Bot.BotComponent.CompSystem import CompSystem
+from Bot.BotComponent.CompUpdateDatabase import CompUpdateDatabase
+from Bot.BotComponent.CompUsers import CompUsers
 
 
 class BotNerolirain(BotBase):
@@ -24,3 +25,4 @@ class BotNerolirain(BotBase):
         self.AddComponent("compClose", CompClose())
         self.AddComponent("compSystem", CompSystem())
         self.AddComponent("compIdiom", CompIdiom())
+        self.AddComponent("compRandom", CompRandom())
